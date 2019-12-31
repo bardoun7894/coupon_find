e388a3002b4ed43e6c845321ca0f7714c0ccee1e
bardouni7894 <mbardouni44@gmail.com>
2020-01-01 00:18:50 +0100

    i finish the first ui

---
+++ lib/Animation/mo.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+
+import 'FindEvent.dart';
+
+class SplashScreen extends StatefulWidget {
+	@override
+	_SplashScreen createState() => new _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+
+	@override
+	void initState() {
+		// TODO: implement initState
+		super.initState();
+		Timer(
+				Duration(seconds: 3),
+						()=> {
+					Navigator.push(
+						context,
+						MaterialPageRoute(builder: (context) => FindEvent()),
+					)
+				}
+		);
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		// TODO: implement build
+		return Scaffold(
+				body: Stack(
+					fit: StackFit.expand,
+					children: <Widget>[
+						Container(
+							decoration: BoxDecoration(color: Colors.red
+							,gradient: LinearGradient(
+										colors: [new Color(0xff6094e8),new Color(0xffde5cbc)]
+												,begin: Alignment.centerRight
+											,end: Alignment.centerLeft
+									)),
+						),
+						Column(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+							Expanded(
+								flex: 2,
+								child: Container(
+										child: Column(
+											mainAxisAlignment: MainAxisAlignment.center,
+											children: <Widget>[
+												CircleAvatar(
+													backgroundColor: Colors.white,
+													radius: 50.0,
+													child: Icon(
+														Icons.confirmation_number,
+														color: Color.fromRGBO( 255, 121, 121 ,1),
+														size: 50.0,
+													),
+												),
+												Padding(
+													padding: EdgeInsets.only(top:10.0),
+												),
+											],
+										)
+								),
+							),
+							Expanded(
+								flex: 1,
+								child: Column(
+									mainAxisAlignment: MainAxisAlignment.center,
+									children: <Widget>[
+										CircularProgressIndicator(),
+										Padding(
+											padding: EdgeInsets.only(top:20.0),
+
+										),
+										Text(
+											"Loading ...",
+											style: TextStyle(
+													color: Colors.white,
+													fontSize: 18.0,
+													fontWeight: FontWeight.bold),
+										)
+									],
+								),
+							)
+						])
+					],
+				));
+	}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,30 @@
-import 'package:coupon_find/Animation/FadeAnimation.dart';
+ 
+ 
+import 'package:coupon_find/Animation/mo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  AnimationController _scaleController;
-  Animation<double> _scaleAnimation;
-  bool hide = false;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _scaleController = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 300),
-    );
-    _scaleAnimation =
-        Tween<double>(begin: 1.0, end: 30.0).animate(_scaleController)
-          ..addStatusListener((status) {
-            if (status == AnimationStatus.completed) {}
-          });
-  }
-
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _scaleController.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage("asset/adv.jpg"),
-        fit: BoxFit.fill,
-      )),
-      child: Container(
-        padding: EdgeInsets.all(30.0),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.bottomCenter, colors: [
-            Colors.black.withOpacity(.9),
-            Colors.black.withOpacity(.6),
-            Colors.black.withOpacity(.8),
-            Colors.black.withOpacity(.3)
-          ]),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            FadeAnimation(
-                1.0,
-                Text("find best Location",
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(.9),
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold))),
-            SizedBox(
-              height: 30.0,
-            ),
-            FadeAnimation(
-                1.2,
-                Text(
-                  "find best Location",
-                  style: TextStyle(
-                      color: Colors.white.withOpacity(.5),
-                      fontSize: 21.0,
-                      fontWeight: FontWeight.bold),
-                )),
-            SizedBox(
-              height: 150,
-            ),
-            FadeAnimation(1.4,InkWell(
-              onTap: () {
-                setState(() {
-                  hide = true;
-                });
-                _scaleController.forward();
-              },
-              child: AnimatedBuilder(
-                animation: _scaleController,
-                builder: (context,child)=>Transform.scale(
-                  scale: _scaleAnimation.value,
-                child:Container(
-                  height: 60,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50.0),
-                      color: Colors.yellow[700]),
-                  child: hide == false ?
-                  Row( mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Text(
-                        "find everset near me ",
-                        style: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                      Icon( Icons.arrow_forward, color: Colors.white, )
-                    ],
-                  ):Container(),
-                ),)
-              )
-
-
-            )),
-            SizedBox(
-              height: 60,
-            )
-          ],
-        ),
-      ),
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
     ));
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale("ar"), // OR Locale('ar', 'AE') OR Other RTL locales
+      ],
+      locale: Locale("ar") // OR Locale('ar', 'AE') OR Other RTL locales,
+
+    ,  debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
   }
 }
